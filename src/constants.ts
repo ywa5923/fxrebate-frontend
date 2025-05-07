efb75580ef8a7e6ba2f34bcc17f15a68ef51db75
Ion Ivan <ivanion10@gmail.com>
2025-05-07 22:26:37 +0300

    changed nginx port number in BASE_URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1";
